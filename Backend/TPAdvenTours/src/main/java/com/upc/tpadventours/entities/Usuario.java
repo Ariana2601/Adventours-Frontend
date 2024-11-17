5f15a318fd8edacb4083952efd8473d1c8e9e7cf
JuanGhost <u202219061@upc.edu.pe>
2024-11-17 16:59:48 -0500

    Update Usuario.java

--- Backend/TPAdvenTours/src/main/java/com/upc/tpadventours/entities/Usuario.java
+++ Backend/TPAdvenTours/src/main/java/com/upc/tpadventours/entities/Usuario.java
@@ -1,12 +1,9 @@
 package com.upc.tpadventours.entities;
-
-
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
 @Setter
 @Getter
 @AllArgsConstructor
@@ -17,21 +14,17 @@ public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idUsuario;
-
     @Column(name = "Nombre_completo", nullable = false, length = 100)
     private String nombreCompleto;
-
     @Column(name = "Nombre_usuario", nullable = false, length = 50)
     private String nombreUsuario;
-
     @Column(name = "Correo_electronico", nullable = false, length = 100)
     private String correoElectronico;
-
     @Column(name = "contrasena", nullable = false, length = 200)
     private String contrasena;
-
     @ManyToOne
     @JoinColumn(name = "id_rol")
     private Rol rol;
-
 }
+
+//Entidad terminada
