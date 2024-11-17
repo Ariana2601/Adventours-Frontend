bfaf053ff17062878a6b545249d1626d437aedca
JuanGhost <u202219061@upc.edu.pe>
2024-11-17 17:07:06 -0500

    Update RolService.java

--- Backend/TPAdvenTours/src/main/java/com/upc/tpadventours/services/RolService.java
+++ Backend/TPAdvenTours/src/main/java/com/upc/tpadventours/services/RolService.java
@@ -1,13 +1,10 @@
 package com.upc.tpadventours.services;
-
 import com.upc.tpadventours.entities.Rol;
-
 import java.util.List;
-
 public interface RolService {
     public Rol insertarRol(Rol rol);
     public void eliminarRol(Long id);
     public List<Rol> listarRol();
     public Rol obtenerRolPorNombre(String nombreRol);
-
+    //completado
 }
