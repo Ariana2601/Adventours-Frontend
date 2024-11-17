139f771bf25a1a35dc5cea883d642a504d374519
JuanGhost <u202219061@upc.edu.pe>
2024-11-17 17:06:14 -0500

    Update ContactoService.java

--- Backend/TPAdvenTours/src/main/java/com/upc/tpadventours/services/ContactoService.java
+++ Backend/TPAdvenTours/src/main/java/com/upc/tpadventours/services/ContactoService.java
@@ -1,10 +1,7 @@
 package com.upc.tpadventours.services;
-
 import com.upc.tpadventours.DtoHUs.HUContactoDTO;
 import com.upc.tpadventours.entities.Contacto;
-
 import java.util.List;
-
 public interface ContactoService {
     public Contacto insertarContacto(Contacto contacto);
     public List<Contacto> listarContactos();
