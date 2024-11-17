702e7d5a6a42c3ad70a27cfb399e5ffe132fb8c9
JuanGhost <u202219061@upc.edu.pe>
2024-11-17 17:01:14 -0500

    Update ContactoDTO.java

--- Backend/TPAdvenTours/src/main/java/com/upc/tpadventours/dtos/ContactoDTO.java
+++ Backend/TPAdvenTours/src/main/java/com/upc/tpadventours/dtos/ContactoDTO.java
@@ -4,15 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
 import java.time.LocalDateTime;
-
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class ContactoDTO {
-
     private long idContacto;
     private String tipoMensaje;
     private String mensaje;
