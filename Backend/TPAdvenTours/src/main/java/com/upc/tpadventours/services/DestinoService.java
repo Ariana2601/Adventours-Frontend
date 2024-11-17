1d15d497cccc724551d87c0c42842ddc6ab06f47
JuanGhost <u202219061@upc.edu.pe>
2024-11-17 17:06:27 -0500

    Update DestinoService.java

--- Backend/TPAdvenTours/src/main/java/com/upc/tpadventours/services/DestinoService.java
+++ Backend/TPAdvenTours/src/main/java/com/upc/tpadventours/services/DestinoService.java
@@ -1,12 +1,9 @@
 package com.upc.tpadventours.services;
-
 import com.upc.tpadventours.DtoHUs.DestinoDetallesDTO;
 import com.upc.tpadventours.DtoHUs.HURecomendacionesDeViajeDto;
 import com.upc.tpadventours.entities.Destino;
 import com.upc.tpadventours.entities.Usuario;
-
 import java.util.List;
-
 public interface DestinoService {
     public Destino insertarDestino(Destino destino);
     public void eliminarDestino(Long id);
@@ -20,5 +17,4 @@ public interface DestinoService {
     public DestinoDetallesDTO obtenerDetallesDestino(Long destinoId);
     public Destino obtenerDestinoPorId(Long id);
     public Destino obtenerDestinoPorNombre(String nombre);
-
 }
