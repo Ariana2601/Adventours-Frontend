6984eeb58b30fbfd706ea28ebae9747e62700a6b
JuanGhost <u202219061@upc.edu.pe>
2024-11-17 17:11:18 -0500

    Update RolServiceImpl.java

--- Backend/TPAdvenTours/src/main/java/com/upc/tpadventours/servicesImplementation/RolServiceImpl.java
+++ Backend/TPAdvenTours/src/main/java/com/upc/tpadventours/servicesImplementation/RolServiceImpl.java
@@ -1,15 +1,12 @@
 package com.upc.tpadventours.servicesImplementation;
-
 import com.upc.tpadventours.Repository.RolRepository;
 import com.upc.tpadventours.entities.Empresa;
 import com.upc.tpadventours.entities.Rol;
 import com.upc.tpadventours.services.RolService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
-
 @Service
 public class RolServiceImpl implements RolService {
     @Autowired
@@ -28,21 +25,18 @@ public class RolServiceImpl implements RolService {
     public List<Rol> listarRol(){
         return rolRepository.findAll();
     }
-
     @Override
     public Rol obtenerRolPorNombre(String nombreRol) {
         // Busca si el rol existe
         Optional<Rol> rolOptional = rolRepository.findByNombreRol(nombreRol);
-
         // Si el rol no existe, lo crea
         if (!rolOptional.isPresent()) {
             Rol nuevoRol = new Rol();
             nuevoRol.setNombreRol(nombreRol);
             return rolRepository.save(nuevoRol);
         }
-
         // Si ya existe, lo retorna
         return rolOptional.get();
     }
-
+//completado
 }
