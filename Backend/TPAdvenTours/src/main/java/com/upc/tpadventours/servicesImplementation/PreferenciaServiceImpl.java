520c180adce559e66e01bdbb3a297a1e2c177bb5
JuanGhost <u202219061@upc.edu.pe>
2024-11-17 17:10:52 -0500

    Update PreferenciaServiceImpl.java

--- Backend/TPAdvenTours/src/main/java/com/upc/tpadventours/servicesImplementation/PreferenciaServiceImpl.java
+++ Backend/TPAdvenTours/src/main/java/com/upc/tpadventours/servicesImplementation/PreferenciaServiceImpl.java
@@ -1,40 +1,31 @@
 package com.upc.tpadventours.servicesImplementation;
-
 import com.upc.tpadventours.Repository.PreferenciasRepository;
 import com.upc.tpadventours.entities.Preferencia;
 import com.upc.tpadventours.services.PreferenciaService;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
-
 @Service
 public class PreferenciaServiceImpl implements PreferenciaService {
     @Autowired
     private PreferenciasRepository preferenciasRepository;
-
     @Override
     public Preferencia insertarPreferencia(Preferencia preferencia) {
         return preferenciasRepository.save(preferencia);
     }
-
-
     @Override
     public List<Preferencia> listarPreferencias() {
         return preferenciasRepository.findAll();
     }
-
     @Override
     public List<Preferencia> buscarPreferenciasPorUsuario(Long usuarioId) {
         return preferenciasRepository.findByUsuarioId(usuarioId);
     }
-
     @Override
     @Transactional
     public void eliminarPreferenciasPorUsuario(Long usuarioId) {
         preferenciasRepository.deleteByUsuarioId(usuarioId);
     }
-
-
 }
+//completado
