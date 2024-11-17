2c8f09d8ee9e184a2f560e9ca4761cec463ae7ea
Denisse1728 <113485667+Denisse1728@users.noreply.github.com>
2024-11-17 18:29:58 -0500

    Update DestinoController.java

--- Backend/TPAdvenTours/src/main/java/com/upc/tpadventours/controller/DestinoController.java
+++ Backend/TPAdvenTours/src/main/java/com/upc/tpadventours/controller/DestinoController.java
@@ -13,20 +13,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.stream.Collectors;
-
 @RestController
 @CrossOrigin(origins = {"*", "http://localhost:4200","http://3.131.140.104/"}, allowedHeaders = "*")
 @RequestMapping("/api")
 public class DestinoController {
     @Autowired
     private DestinoService destinoService;
-
     @Autowired
     private EmpresaService empresaService;
-
     //COMPLETO
     @PostMapping("/destinos/insertarDestino")
     //@PreAuthorize("hasAnyRole('ADMIN')")
@@ -36,7 +32,6 @@ public class DestinoController {
         destino = destinoService.insertarDestino(destino);
         return modelMapper.map(destino, DestinoDTO.class);
     }
-
     //COMPLETO
     @PostMapping("/destinos/asignarDestinoEmpresa")
     //@PreAuthorize("hasAnyRole('ADMIN')")
@@ -48,7 +43,6 @@ public class DestinoController {
         destino = destinoService.actualizarDestino(destino);
         return modelMapper.map(destino, DestinoDTO.class);
     }
-
     //COMPLETO - REQUIERE EL ID EN EL POSTMAN
     @PutMapping("/destinos/modificarDestino")
     //@PreAuthorize("hasAnyRole('ADMIN')")
@@ -58,21 +52,18 @@ public class DestinoController {
         destino = destinoService.modificarDestino(destino);
         return modelMapper.map(destino, DestinoDTO.class);
     }
-
     //COMPLETO
     @GetMapping("/destinos/listarDestinos")
     //@PreAuthorize("hasAnyRole('ADMIN','USER')")
     public List<Destino> listarDestinos() {
         return destinoService.listarDestino();
     }
-
     //COMPLETO
     @DeleteMapping("/destinos/eliminarDestino/{id}")
     //@PreAuthorize("hasAnyRole('ADMIN')")
     public void eliminarDestino(@PathVariable Long id) {
         destinoService.eliminarDestino(id);
     }
-
     //COMPLETO
     @GetMapping("/destinos/DestinosRecomendaciones/{usuarioId}")
     //@PreAuthorize("hasAnyRole('USER')")
@@ -80,7 +71,6 @@ public class DestinoController {
         List<HURecomendacionesDeViajeDto> recomendaciones = destinoService.obtenerMejoresDestinos(usuarioId);
         return ResponseEntity.ok(recomendaciones);
     }
-
     //COMPLETO
     @GetMapping("/destinos/DestinosPopulares")
     //@PreAuthorize("hasAnyRole('USER')")
@@ -92,7 +82,6 @@ public class DestinoController {
                 .collect(Collectors.toList());
         return ResponseEntity.ok(popularesDto);
     }
-
     @GetMapping("/destinos/obtenerDestinoPorId/{id}")
     //@PreAuthorize("hasAnyRole('ADMIN')")
     public DestinoDTO listarDestinos(@PathVariable Long id) {
@@ -100,7 +89,6 @@ public class DestinoController {
         Destino destino = destinoService.obtenerDestinoPorId(id);
         return modelMapper.map(destino, DestinoDTO.class);
     }
-
     @GetMapping("/destinos/obtenerDestinoporNombre/{nombre}")
     //@PreAutorize("hasAnyRole('ADMIN')")
     public DestinoDTO listarDestinoPorNombre(@PathVariable String nombre) {
@@ -108,4 +96,5 @@ public class DestinoController {
         Destino destino = destinoService.obtenerDestinoPorNombre(nombre);
         return modelMapper.map(destino, DestinoDTO.class);
     }
+    //completo
 }
