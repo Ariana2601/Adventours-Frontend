7482864b48a7eaadd4421da5b1792830a880b3bc
JuanGhost <u202219061@upc.edu.pe>
2024-11-17 17:01:33 -0500

    Update DestinoDTO.java

--- Backend/TPAdvenTours/src/main/java/com/upc/tpadventours/dtos/DestinoDTO.java
+++ Backend/TPAdvenTours/src/main/java/com/upc/tpadventours/dtos/DestinoDTO.java
@@ -1,14 +1,11 @@
 package com.upc.tpadventours.dtos;
-
 import com.upc.tpadventours.entities.Empresa;
 import com.upc.tpadventours.entities.Usuario;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
 import java.util.Set;
-
 @Getter
 @Setter
 @AllArgsConstructor
