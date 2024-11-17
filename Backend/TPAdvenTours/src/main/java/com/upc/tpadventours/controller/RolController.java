bea37903fc827a305e445714286129eaa4b4d7be
Denisse1728 <113485667+Denisse1728@users.noreply.github.com>
2024-11-17 18:31:16 -0500

    Update RolController.java

--- Backend/TPAdvenTours/src/main/java/com/upc/tpadventours/controller/RolController.java
+++ Backend/TPAdvenTours/src/main/java/com/upc/tpadventours/controller/RolController.java
@@ -1,5 +1,4 @@
 package com.upc.tpadventours.controller;
-
 import com.upc.tpadventours.dtos.PreferenciaDTO;
 import com.upc.tpadventours.dtos.RolDTO;
 import com.upc.tpadventours.entities.Preferencia;
@@ -9,16 +8,13 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
-
 @RestController
 @CrossOrigin(origins = {"*", "http://localhost:4200","http://3.131.140.104/"}, allowedHeaders = "*")
 @RequestMapping("/api")
 public class RolController {
     @Autowired
     private RolService rolService;
-
     //Pruebas - no es necesario
     @PostMapping("/rol")
     //@PreAuthorize("hasAnyRole('ADMIN')")
@@ -27,13 +23,11 @@ public class RolController {
         Rol rol  = modelMapper.map(rolDTO, Rol.class);
         rol = rolService.insertarRol(rol);
         return modelMapper.map(rol, RolDTO.class);
-
     }
     //Pruebas - no es necesario
     @GetMapping("/listarrol")
     //@PreAuthorize("hasAnyRole('ADMIN')")
     public List<Rol> listarRoles() {return rolService.listarRol();}
-
     //Pruebas - no es necesario
     @DeleteMapping("/eliminarrol")
     //@PreAuthorize("hasAnyRole('ADMIN')")
@@ -42,6 +36,5 @@ public class RolController {
         Rol rol  = modelMapper.map(rolDTO, Rol.class);
         rolService.eliminarRol(rol.getId());
     }
-
-
 }
+//completo
