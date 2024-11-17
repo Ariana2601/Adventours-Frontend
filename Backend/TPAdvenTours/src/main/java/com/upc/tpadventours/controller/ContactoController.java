63b0634d4fe9501e2704a167e057507a305c8fc2
Denisse1728 <113485667+Denisse1728@users.noreply.github.com>
2024-11-17 18:29:32 -0500

    Update ContactoController.java

--- Backend/TPAdvenTours/src/main/java/com/upc/tpadventours/controller/ContactoController.java
+++ Backend/TPAdvenTours/src/main/java/com/upc/tpadventours/controller/ContactoController.java
@@ -1,5 +1,4 @@
 package com.upc.tpadventours.controller;
-
 import com.upc.tpadventours.DtoHUs.HUContactoDTO;
 import com.upc.tpadventours.dtos.ContactoDTO;
 import com.upc.tpadventours.entities.Contacto;
@@ -12,16 +11,13 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import java.time.LocalDateTime;
 import java.util.List;
-
 //@CrossOrigin(origins = {"http://localhost:4200","http://3.131.140.104/"})
 @RestController
 @CrossOrigin(origins = {"*", "http://localhost:4200","http://3.131.140.104/"}, allowedHeaders = "*")
 @RequestMapping("/api")
-
 public class ContactoController {
     @Autowired
     private ContactoService contactoService;
-
     //COMPLETO - PRUEBA
     @PostMapping("/contactos/insertarContactos")
     //@PreAuthorize("hasAnyRole('ADMIN')")
@@ -31,14 +27,12 @@ public class ContactoController {
         contacto = contactoService.insertarContacto(contacto);
         return modelMapper.map(contacto, ContactoDTO.class);
     }
-
-    //COMPLETO
+    //COmPLETO
     @GetMapping("/contactos/listarContactos")
     //@PreAuthorize("hasAnyRole('ADMIN')")
     public List<Contacto> listarContactos() {
         return contactoService.listarContactos();
     }
-
     //COMPLETO
     @PostMapping("/contactos/enviarMensaje")
     //@PreAuthorize("hasAnyRole('USER')")
@@ -50,6 +44,5 @@ public class ContactoController {
         HUContactoDTO contactoDTO = modelMapper.map(mensajeGuardado, HUContactoDTO.class);
         return ResponseEntity.ok(contactoDTO);
     }
-
-//Aqui se termino el codigo - verificado
+//completo
 }
