624bf5549cb7fa4d31a0fc27f60cc1db9088ff69
JuanGhost <u202219061@upc.edu.pe>
2024-11-17 17:03:28 -0500

    Update PreferenciaDTO.java

--- Backend/TPAdvenTours/src/main/java/com/upc/tpadventours/dtos/PreferenciaDTO.java
+++ Backend/TPAdvenTours/src/main/java/com/upc/tpadventours/dtos/PreferenciaDTO.java
@@ -1,16 +1,13 @@
 package com.upc.tpadventours.dtos;
-
 import com.upc.tpadventours.entities.Usuario;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-
 public class PreferenciaDTO {
     private long idPreferencia;
     private String clima;
