fc69a6429c133fa5d63b020ecc9bac0c856170a4
JuanGhost <u202219061@upc.edu.pe>
2024-11-17 17:07:25 -0500

    Update UsuarioService.java

--- Backend/TPAdvenTours/src/main/java/com/upc/tpadventours/services/UsuarioService.java
+++ Backend/TPAdvenTours/src/main/java/com/upc/tpadventours/services/UsuarioService.java
@@ -1,13 +1,10 @@
 package com.upc.tpadventours.services;
-
 import com.upc.tpadventours.DtoHUs.HUIngresoSistema;
 import com.upc.tpadventours.DtoHUs.HURegistroDatos;
 import com.upc.tpadventours.entities.Usuario;
 import org.springframework.data.repository.query.Param;
-
 import java.util.List;
 import java.util.Optional;
-
 public interface UsuarioService {
     public Usuario insertarUsuario(Usuario usuario);
     public void eliminarUsuario(Long id);
@@ -17,4 +14,4 @@ public interface UsuarioService {
     public List<HUIngresoSistema> BuscarPorNombreUsuarioYContraseña(@Param("nombreUsuario") String nombreUsuario, @Param("contraseña") String contraseña);
     public Optional<HURegistroDatos> findByNombreUsuarioOrCorreo(String nombreUsuario, String correoElectronico);
 }
-
+//completado
